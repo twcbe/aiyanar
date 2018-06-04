a34abeaca3d6093c48a86dbdee422e4cca57b501
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-06-04 12:25:55 +0530

    Allow choosing cards and roles while editting users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
-  permit_params :employee_id, :name
+  permit_params :employee_id, :name, card_ids: [], role_ids: []
 #
 # or
 #
@@ -17,25 +17,35 @@ ActiveAdmin.register User do
 
     panel "Issued Cards" do
       table_for(user.cards) do
-        column("Card") do |card|
+        column("Cards") do |card|
           link_to "#{card.card_number}", admin_card_path(card)
         end
       end unless user.cards.empty?
       b "No cards issued" if user.cards.empty?
     end
 
+    panel "Assigned Roles" do
+      table_for(user.roles) do
+        column("Roles") do |role|
+          link_to "#{role.name}", admin_role_path(role)
+        end
+      end unless user.roles.empty?
+      b "No roles assigned" if user.roles.empty?
+    end
+
     attributes_table(*default_attribute_table_rows)
   end
 
   form do |f|
-    f.inputs do
+    f.inputs "Details" do
       if user.new_record?
-        f.input :employee_id
+        f.input :employee_id, label: "Employee Id"
       else
-        f.input :employee_id, input_html: { disabled: true }
+        f.input :employee_id, label: "Employee Id", input_html: { disabled: true }
       end
       f.input :name
-      f.input :roles, as: :check_boxes, collection: Role.all
+      f.input :cards
+      f.input :roles
     end
     f.actions
   end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ApplicationRecord
   belongs_to :user, optional: true
+
+  def to_s
+    card_number
+  end
+
 end
