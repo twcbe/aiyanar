47546ea6d3e24844af4e49ad2e63f9a8e73f1e40
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-06-07 14:04:37 +0530

    Make the mqtt message handling server more robust

--- config/initializers/mqtt_handler.rb
+++ config/initializers/mqtt_handler.rb
@@ -1,17 +1,39 @@
 CARD_READER_TOPIC = 'access_control/card_readers'
-MQTT_BROKER_HOST = 'localhost'
+MQTT_BROKER_HOST = '10.137.120.19'
 MQTT_BROKER_PORT = 1883
 
-Thread.new do
-
-  mqtt_client = MqttClient.new(host = MQTT_BROKER_HOST, port = MQTT_BROKER_PORT)
-  Rails.logger.info '[mqtt_handler] Connected to broker'
-
-  mqtt_router = MqttRouter.new(CardHandler.new(mqtt_client))
-
-  mqtt_client.subscribe_and_run CARD_READER_TOPIC do |_, payload|
-    mqtt_router.handle payload
+def log_and_ignore_exception
+  begin
+    yield
+  rescue Exception => e
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace.join("\n")
+    # ignored
   end
+end
 
+class MqttHandler
+  def run
+    mqtt_client = MqttClient.new(host = MQTT_BROKER_HOST, port = MQTT_BROKER_PORT)
+    Rails.logger.info '[mqtt_handler] Connected to broker'
+
+    mqtt_router = MqttRouter.new(CardHandler.new(mqtt_client))
+
+    mqtt_client.subscribe_and_run CARD_READER_TOPIC do |_, payload|
+      log_and_ignore_exception do
+        mqtt_router.handle payload
+      end
+    end
+  end
+end
+
+Thread.new do
+  while true
+    Rails.logger.info '[mqtt_handler] Starting up'
+    log_and_ignore_exception do
+      MqttHandler.new.run
+    end
+    Rails.logger.info '[mqtt_handler] Restarting due to previous exception'
+  end
 end
 
