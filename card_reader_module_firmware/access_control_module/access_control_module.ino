3f85234a4ca17b66abf1e66086d72b44914a099f
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-06-14 19:19:15 +0530

    [Ashok/Sachin] Firmware: Unlock door based on server message for specified duration. Handle emergency mode.

---
+++ card_reader_module_firmware/access_control_module/access_control_module.ino
@@ -0,0 +1,284 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <Wiegand.h>
+#include <TaskScheduler.h>
+
+// customizable options:
+#define CARD_READER_TOPIC "access_control/card_readers"
+#define SERVER_TOPIC      "access_control/server"
+
+#define DOOR_UNLOCK_MIN_DURATION 1000
+#define DOOR_UNLOCK_MAX_DURATION 30000
+
+const char* ssid = "twguest";
+const char* password = "crimson trout rewrite trustable ivy";
+const char* mqttServer = "10.137.120.19";
+// END customizable options
+
+#define d0 16
+#define d1 5
+#define d2 4
+#define d3 0
+#define d4 2
+#define d5 14
+#define d6 12
+#define d7 13
+#define d8 15
+
+#define ENTRY_CARD_READER_DATA0 d7
+#define ENTRY_CARD_READER_DATA1 d8
+#define ENTRY_CARD_READER_LED   d6
+#define ENTRY_CARD_READER_BEEP  d5
+#define EXIT_CARD_READER_DATA0  d3
+#define EXIT_CARD_READER_DATA1  d4
+#define EXIT_CARD_READER_LED    d2
+#define EXIT_CARD_READER_BEEP   d1
+#define LOCK_RELAY              d0
+
+#define DOOR_UNLOCKED LOW
+#define DOOR_LOCKED   HIGH
+#define BEEP_OFF      HIGH
+#define BEEP_ON       LOW
+#define LED_RED       HIGH
+#define LED_GREEN     LOW
+
+#define clamp(value, min, max) ((value) > (max) ? (max) : ( (value) < (min) ? (min) : (value) ))
+
+enum class LockStates
+{
+  LOCKED,
+  UNLOCKED_FOR_DURATION,
+  EMERGENCY_UNLOCKED
+};
+
+void lockDoorCallback();
+
+Scheduler taskRunner;
+Task lockDoorTask(0, 1, &lockDoorCallback);
+WIEGAND entryCardReader;
+WIEGAND exitCardReader;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+LockStates lockState = LockStates::LOCKED;
+
+int asInteger(LockStates lockState)
+{
+    return static_cast<std::underlying_type<LockStates>::type>(lockState);
+}
+
+void stateChangeTo(LockStates newLockState) {
+  lockState = newLockState;
+  Serial.print("Lock state changed to ");
+  Serial.println(asInteger(newLockState));
+}
+
+void lockDoorCallback() {
+  Serial.println("Locking door.");
+  digitalWrite(LOCK_RELAY, DOOR_LOCKED);
+  digitalWrite(ENTRY_CARD_READER_LED, LED_RED);
+  stateChangeTo(LockStates::LOCKED);
+}
+
+void lockDoorAfter(unsigned long durationMillis) {
+  lockDoorTask.restartDelayed(durationMillis);
+}
+
+void unlockDoorFor(unsigned long durationMillis) {
+  Serial.print("Unlocking door for ");
+  Serial.print(durationMillis);
+  Serial.println(" milliseconds");
+  digitalWrite(LOCK_RELAY, DOOR_UNLOCKED);
+  digitalWrite(ENTRY_CARD_READER_LED, LED_GREEN);
+  lockDoorAfter(durationMillis);
+  stateChangeTo(LockStates::UNLOCKED_FOR_DURATION);
+}
+
+void enterEmergencyMode() {
+  Serial.print("Entering emergency mode. Unlocking door.");
+  digitalWrite(LOCK_RELAY, DOOR_UNLOCKED);
+  stateChangeTo(LockStates::EMERGENCY_UNLOCKED);
+  lockDoorTask.disable();
+  digitalWrite(ENTRY_CARD_READER_LED, LED_GREEN);
+}
+
+void exitEmergencyMode(unsigned long durationMillis) {
+  Serial.print("Exiting emergency mode. Locking door in ");
+  Serial.print(durationMillis);
+  Serial.println(" milliseconds");
+  lockDoorAfter(durationMillis);
+  stateChangeTo(LockStates::UNLOCKED_FOR_DURATION);
+}
+
+void setup() {
+
+  pinMode(ENTRY_CARD_READER_BEEP, OUTPUT);
+  pinMode(ENTRY_CARD_READER_LED,  OUTPUT);
+  pinMode(EXIT_CARD_READER_BEEP,  OUTPUT);
+  pinMode(EXIT_CARD_READER_LED,   OUTPUT);
+
+  pinMode(LOCK_RELAY, OUTPUT);
+
+  digitalWrite(ENTRY_CARD_READER_BEEP, BEEP_OFF); // HIGH=beep_off    LOW=beep_on
+  digitalWrite(ENTRY_CARD_READER_LED,  LED_RED);  // HIGH=red         LOW=green
+  digitalWrite(EXIT_CARD_READER_BEEP,  BEEP_OFF); // HIGH=beep_off    LOW=beep_on
+  digitalWrite(EXIT_CARD_READER_LED,   LED_RED);  // HIGH=red         LOW=green
+
+  delay(1000);
+  digitalWrite(LOCK_RELAY, DOOR_LOCKED); // HIGH=door_locked LOW=door_unlocked
+  delay(1000);
+  digitalWrite(LOCK_RELAY, DOOR_UNLOCKED); // HIGH=door_locked LOW=door_unlocked
+  delay(1000);
+  digitalWrite(LOCK_RELAY, DOOR_LOCKED); // HIGH=door_locked LOW=door_unlocked
+
+  Serial.begin(115200);
+  setupWifi();
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
+  entryCardReader.begin(ENTRY_CARD_READER_DATA0, ENTRY_CARD_READER_DATA1);
+  exitCardReader.begin(EXIT_CARD_READER_DATA0,   EXIT_CARD_READER_DATA1);
+
+  taskRunner.init();
+  taskRunner.addTask(lockDoorTask);
+}
+
+void setupWifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.publish(CARD_READER_TOPIC, "[Main entrance] reconnected");
+      client.subscribe(SERVER_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2 seconds");
+      delay(2000);
+    }
+  }
+}
+
+void publishCardReadMessage(unsigned long cardNumber, char* direction) {
+  char cardNumberHex[100]="";
+  snprintf(cardNumberHex, sizeof(cardNumberHex), "%lX", cardNumber);
+  char payload[250]={};
+  String strBuffer;
+  DynamicJsonBuffer  jsonBuffer(200);
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["message"] = "card_read";
+  root["lock_name"] = "Main entrance";
+  root["direction"] = direction;
+  root["card_number"] = cardNumberHex;
+  root.printTo(strBuffer);
+  strBuffer.toCharArray(payload, sizeof(payload));
+
+  client.publish(CARD_READER_TOPIC, payload);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  //handle messages
+
+  DynamicJsonBuffer  jsonBuffer(200);
+  JsonObject& root = jsonBuffer.parseObject((const char*) payload);
+  const char *message = root["message"];
+  const char *lockName = root["lock_name"];
+  const char *command = root["command"];
+
+  if (strcmp(command, "start_emergency")==0)
+  {
+    enterEmergencyMode();
+  }
+  //state machine
+  switch(lockState) {
+    case LockStates::LOCKED:
+      if (strcmp(command, "open_door")==0)
+      {
+        float duration = root["duration"];
+        duration = duration*1000;
+        unsigned long unlockDuration = clamp(duration, DOOR_UNLOCK_MIN_DURATION, DOOR_UNLOCK_MAX_DURATION);
+        unlockDoorFor(unlockDuration);
+      }
+      break;
+    case LockStates::UNLOCKED_FOR_DURATION:
+      if (strcmp(command, "open_door")==0)
+      {
+        float duration = root["duration"];
+        duration = duration*1000;
+        unsigned long unlockDuration = clamp(duration, DOOR_UNLOCK_MIN_DURATION, DOOR_UNLOCK_MAX_DURATION);
+        unlockDoorFor(unlockDuration);
+      }
+      break;
+    case LockStates::EMERGENCY_UNLOCKED:
+      if (strcmp(command, "end_emergency")==0)
+      {
+        float duration = root["duration"];
+        float invalid_value = root["invalid_key"];
+        Serial.println(invalid_value);
+        duration = duration*1000;
+        unsigned long unlockDuration = clamp(duration, DOOR_UNLOCK_MIN_DURATION, DOOR_UNLOCK_MAX_DURATION);
+        exitEmergencyMode(unlockDuration);
+      }
+      break;
+  }
+
+//  - server to CR {"command": "open_door", "duration": 5, "beep_tone": "twice", "lock_name": "main_door" } - whenever card is swiped
+//  - server to CR {"command": "deny_access", "beep_tone": "something", "feedback_led": "toggle_twice"}
+//
+}
+
+void loop() {
+  //MQTT
+  if (!client.connected()) {
+    Serial.println("MQTT not connected");
+    reconnect();
+  }
+  client.loop();
+
+  //card reader 1
+  if (entryCardReader.available()) {
+    unsigned long cardNumber = entryCardReader.getCode();
+    publishCardReadMessage(cardNumber, "enter");
+  }
+
+  //card reader 2
+  if (exitCardReader.available()) {
+    unsigned long cardNumber = exitCardReader.getCode();
+    publishCardReadMessage(cardNumber, "exit");
+  }
+
+  taskRunner.execute();
+  delay(100);
+}
+
+
+
+
+
+
+
+
+
+
