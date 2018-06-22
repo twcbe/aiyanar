c027195a1f55d5ebae98b907cd238e9a741694b4
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-06-22 15:47:55 +0530

    [Ashok/Sachin] Don't use encrypted secrets. provide secret key base via env variable for production

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BackendService
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.read_encrypted_secrets = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
