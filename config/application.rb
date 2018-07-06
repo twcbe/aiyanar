a326d48f89a6fcd054c5bee02674fcf9e3df79c5
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-07-06 12:34:26 +0530

    [Ashok/Sachin/Selva] Run db migration on startup

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module BackendService
     config.load_defaults 5.2
     config.read_encrypted_secrets = false
 
+    config.after_initialize do
+      ActiveRecord::Tasks::DatabaseTasks.migrate
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
