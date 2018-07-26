4c20d08479cf5cf7e8e14216fb7f5a23c907ee88
Sachin <sachinp@thoughtworks.com>
2018-07-26 15:21:59 +0530

    [Ashok/Sachin] Fix access control backend issue: Trying to add user with duplicated employee id crash system.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, allow_blank: false
+  validates_uniqueness_of :employee_id, :allow_blank => true
   has_many :cards
   has_and_belongs_to_many :roles
 
