192c6bb06b9fef95cc585ca486e56f1cf468ae0e
Ashok Gowtham M <ashokgowtham123@gmail.com>
2018-08-27 18:17:36 +0530

    [Ashok] Added pcb design for access control module.

--- card_reader_module/access_control_module/access_control_module.ino
+++ card_reader_module/access_control_module/access_control_module.ino
@@ -48,6 +48,17 @@ const char* mqttServer = "10.137.120.19";
 #define EXIT_CARD_READER_BEEP   d1
 #define LOCK_RELAY              d0
 
+// new PCB layout
+// #define ENTRY_CARD_READER_DATA0 d7
+// #define ENTRY_CARD_READER_DATA1 d6
+// #define ENTRY_CARD_READER_LED   d8
+// #define ENTRY_CARD_READER_BEEP  d5
+// #define EXIT_CARD_READER_DATA0  d4
+// #define EXIT_CARD_READER_DATA1  d3
+// #define EXIT_CARD_READER_LED    d2
+// #define EXIT_CARD_READER_BEEP   d1
+// #define LOCK_RELAY              d0
+
 #define DOOR_UNLOCKED LOW
 #define DOOR_LOCKED   HIGH
 #define BEEP_OFF      HIGH
