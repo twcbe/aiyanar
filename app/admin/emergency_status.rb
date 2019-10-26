b5ab72f1a4a5b3cdf3916e24d18b88ad189989ae
Sreekesh S <sreekesh.s@thoughtworks.com>
2019-10-26 22:55:00 +0530

    [Sreekesh] Adds emergency status support.

---
+++ app/admin/emergency_status.rb
@@ -0,0 +1,6 @@
+ActiveAdmin.register EmergencyStatus do
+	config.clear_action_items!
+	config.batch_actions = false
+	actions :index
+	
+end
--- config/initializers/mqtt_handler.rb
+++ config/initializers/mqtt_handler.rb
@@ -1,5 +1,6 @@
 CARD_READER_TOPIC = 'access_control/card_readers'
 FACE_RECOGNITION_TOPIC = 'face_recognition'
+EMERGENCY_TOPIC = 'access_control/server'
 MQTT_BROKER_HOST = ENV["MQTT_HOST"] || Settings.mqtt_host
 MQTT_BROKER_PORT = ENV["MQTT_PORT"] || Settings.mqtt_port
 
@@ -18,7 +19,7 @@ class MqttHandler
     mqtt_client = MqttClient.new(host = MQTT_BROKER_HOST, port = MQTT_BROKER_PORT)
     Rails.logger.info '[mqtt_handler] Connected to broker'
 
-    topic_handlers = {CARD_READER_TOPIC => CardHandler.new(mqtt_client), FACE_RECOGNITION_TOPIC => FaceHandler.new(mqtt_client)}
+    topic_handlers = {CARD_READER_TOPIC => CardHandler.new(mqtt_client), FACE_RECOGNITION_TOPIC => FaceHandler.new(mqtt_client), EMERGENCY_TOPIC => EmergencyStatusHandler.new(mqtt_client)}
     mqtt_router = MqttRouter.new(topic_handlers)
 
     topic_handlers.keys.each do |topic|
